d2c3ca37f3a712bd64aef93e1c443cd92b66431d
MelihCelik00 <melihsafa.c@gmail.com>
2020-12-12 17:04:58 +0300

    Tried project for education

---
+++ GeoEduDeneme/Assets/Scripts/Loot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    private void Update()
+    {
+        this.transform.Rotate(new Vector3(5,10,17)*Time.deltaTime);
+    }
+}
---
+++ GeoEduDeneme/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody physics;
+    public int speed;
+
+    public Text CountText;
+    private int pointCounter;
+    public int totalCount;
+    private Time _time;
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+        totalCount = GameObject.FindGameObjectsWithTag("Loot").Length;
+    }
+    
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 vec = new Vector3(horizontal,0,vertical);
+        physics.AddForce(vec*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Loot")
+        {
+            Destroy(other.gameObject);
+            pointCounter++;
+            CountText.text = "Point: " + pointCounter;
+        }
+
+        if (pointCounter==totalCount)
+        {
+            CountText.text = "Game is over!!!!!!";
+            Time.timeScale = 0;
+        }
+    }
+
+}
